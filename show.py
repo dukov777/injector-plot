4bc3918e27834657a96a80a53e827754713e9b9f
Petar Lalov <p.lalov@virtualopensystems.com>
2015-08-01 11:27:06 +0300

    Unification of remote and local plotting
    The plotting is splited in Canvas and Plotter.
    Such split helps for cleaner separation of the concern betwween
    actor and drawer.

--- show.py
+++ show.py
@@ -1,85 +1,74 @@
 #!/usr/bin/env python
 
-import usb1
-import time
-import sys
 import struct 
 import argparse
-from pylab import *
-from numpy  import *
-
-import plotly.plotly as py
-from plotly.graph_objs import *
 
 INJECTORS = 4
-USB_LINE_SIZE = 64
-USB_LINE_HEADER_SIZE = 8
 
 
-
-def find_packet_start(data):
-    for i in range(0, USB_LINE_SIZE*2):
-        timestamp = struct.unpack_from("<L", bytearray(data), i)
-        timestamp2 = struct.unpack_from("<L", bytearray(data), i + INJECTORS)
-        if timestamp == timestamp2:
-            return data[i:]
-    raise Exception("Packet is demaged")
-
-
-def extract_injectors(data): 
-    injectors = []
-    for inj_nr in range(INJECTORS):
-        injector = []
-        for i in range(0, len(data), USB_LINE_SIZE):
-            pack = data[i+USB_LINE_HEADER_SIZE:i+USB_LINE_SIZE]
-            result = map(lambda x: struct.unpack("<B", bytearray(x))[0], pack)
-            injector += result[inj_nr:len(result):INJECTORS]
-        injectors.append(injector)
-    return injectors
+class ICanvas:
+    def plot(self, channel_name, samples):
+        pass    
     
+    def show(self):
+        pass
 
-def plot_injectors_local(injectors):
-    traces = []
-    labels = []
-    for id, injector in injectors:
-    	plot(injector[0:len(injector):args.scale])
-    	labels.append(id)
-                    
-    title(r'Injectors', fontsize=20)
-    xlabel(r"time")
-    legend(labels, bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
-    show()    
 
-def plot_injectors_remote(channels):
+class RemoteCanvas(ICanvas):
     traces = []
-    for channel in channels:
-        data = channel.read()
-        print "Input file size is: ", len(data), " bytes"
-        print "Plotting ", len(data), " bytes"
-        data = data[0:len(data):args.scale]
-        traces.append(
+    def plot(self, channel_name, samples):
+        self.traces.append(
             Scatter(
-                    x=range(0, len(data)),
-                    y=[struct.unpack('<B', x)[0] for x in data]
+                    x=range(0, len(samples)),
+                    y=samples
                 ))
     
-    data = Data(traces)
-
-    layout = Layout(
-        yaxis=YAxis(
-            range=[0, 255],
-            autorange=False,
-            rangemode = ['tozero' , 'nonnegative'],
-        )
-    )
-    fig = Figure(data=data, layout=layout)
-    unique_url = py.plot(fig, filename = 'basic-injector')
+    def show(self):
+        data = Data(self.traces)
     
+        layout = Layout(
+            yaxis=YAxis(
+                range=[0, 255],
+                autorange=False,
+                rangemode = ['tozero' , 'nonnegative'],
+            )
+        )
+        fig = Figure(data=data, layout=layout)
+        unique_url = py.plot(fig, filename = 'basic-injector')
+
+
+class LocalCanvas(ICanvas):
+    labels = []
+    def plot(self, channel_name, samples):
+        plot(samples)
+        self.labels.append(channel_name)
+    
+    def show(self):
+        title(r'Injectors', fontsize=20)
+        xlabel(r"time")
+        legend(self.labels, bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+               ncol=2, mode="expand", borderaxespad=0.)
+        show()    
+
+
+def plot_injectors(channels, canvas):
+    traces = []
+    labels = []
+    for channel_name, channel in channels:
+        samples = channel.read()
+        #scale down the samples array
+        samples = samples[::args.scale]
+        #convert from binary to normal form
+        samples = map(lambda sample: struct.unpack('<B', sample)[0], samples)
+        
+        canvas.plot(channel_name, samples)
+    canvas.show()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Show Injetor Value.')
 
-    parser.add_argument('--injector', dest='injector', default='all', 
+    parser.add_argument('--injector', dest='injector', default='all',
 	                    choices=['0', '1', '2', '3', 'all'],
 	                    help='Injector number [0..3, all] (default: all)')
 
@@ -95,33 +84,22 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    f = open(args.filename)
-    data = f.read()
-    f.close()
-
-    data = find_packet_start(data)
-    injectors = extract_injectors(data)
-
-    if args.injector != 'all':
-        injector = ('injector {}'.format(args.injector), 
-                    injectors[int(args.injector)])
-        injectors = []
-        injectors.append(injector)
-    else:
-        temp = []
-        for i, injector in enumerate(injectors):
-            temp.append(('injector {}'.format(i), injector))
-        injectors = temp      
-    
-    if args.plot == 'local':
-        plot_injectors_local(injectors)
-    else:
-        channels = []
+    channels = []
         
-        if args.injector == "all":
-            for channel in range(INJECTORS):
-                channels.append(open(args.filename + '.' + str(channel), 'r'))
-        else:
-            channels.append(open(args.filename + '.' + args.injector, 'r'))
-        plot_injectors_remote(channels)
+    if args.injector == "all":
+        for channel in range(INJECTORS):
+            channels.append(('injector {}'.format(channel),
+                             open(args.filename + '.' + str(channel), 'r')))
+    else:
+        channels.append(('injector {}'.format(args.injector),
+                         open(args.filename + '.' + args.injector, 'r')))
 
+    if args.plot == 'local':
+        from pylab import *
+        canvas = LocalCanvas()
+    else:
+        import plotly.plotly as py
+        from plotly.graph_objs import *
+        canvas = RemoteCanvas()
+    
+    plot_injectors(channels, canvas)
